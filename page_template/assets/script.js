bad3543f4fd633c172df72e78caa327c6a96bcbb
theSnikk <83441160+theSnikk@users.noreply.github.com>
2023-05-23 08:03:19 +0300

    12

---
+++ page_template/assets/script.js
@@ -0,0 +1,4 @@
+function slide() {
+    var slide = event.target.parentNode.querySelector(".slider__slide").offsetWidth;
+    var slide = event.target.parentNode.querySelector(".slider__slide").offsetWidth
+}
